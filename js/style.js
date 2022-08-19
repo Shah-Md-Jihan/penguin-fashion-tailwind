0ad01b0167815e4c32ea987a7d164b435de63454
Shah-Md-Jihan <shahmohammadjihan@gmail.com>
2022-08-19 11:22:54 +0600

    some js interaction added

---
+++ js/style.js
@@ -0,0 +1,4 @@
+document.getElementById('slide-btn').addEventListener('click', function () {
+    const slideProposalElement = document.getElementById('slide-proposal');
+    slideProposalElement.innerText = 'I AM PANGUIN';
+});
